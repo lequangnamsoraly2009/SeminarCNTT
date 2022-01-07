b1272ea169dde32cf2f80870ad430d0c141c4064
soraly <lequangnam1617@gmail.com>
2022-01-07 16:34:20 +0700

    setup

---
+++ src/routes/index.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { Layout } from "antd";
+import { Route, Routes } from "react-router-dom";
+import HeaderNav from "../components/header";
+import FooterNav from "../components/footer";
+import HomePage from "../pages/HomePage";
+
+const { Content } = Layout;
+
+function PublicRouter() {
+  return (
+    <Layout>
+      <HeaderNav />
+      <Content>
+        <Routes>
+          <Route path="/" exact component={HomePage} />
+        </Routes>
+      </Content>
+      <FooterNav />
+    </Layout>
+  );
+}
+
+export default PublicRouter;
